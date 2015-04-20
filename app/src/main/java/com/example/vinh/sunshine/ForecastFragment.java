d0b3cf85612388c1cc753a28305039b9a0e725ef
Frostlight <cloudy.sonata@gmail.com>
2015-04-20 15:00:43 -0400

    Use preferences for fetching weather

--- app/src/main/java/com/example/vinh/sunshine/ForecastFragment.java
+++ app/src/main/java/com/example/vinh/sunshine/ForecastFragment.java
@@ -1,9 +1,11 @@
 package com.example.vinh.sunshine;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.text.format.Time;
 import android.util.Log;
@@ -62,8 +64,11 @@ public class ForecastFragment extends Fragment {
         // Handle item selection
         switch (item.getItemId()) {
             case R.id.action_refresh:
-                new FetchWeatherTask().execute("94043");
-                return true;
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+                String location = prefs.getString(Integer.toString(R.string.pref_location_key),
+                        Integer.toString(R.string.pref_location_default));
+                new FetchWeatherTask().execute(location);
+            return true;
             //case R.id.action_settings:
             //    return true;
             default:
