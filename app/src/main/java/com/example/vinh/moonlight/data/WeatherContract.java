417ccf864b4c631d9fb28f203128e222f72843fc
Vincent <cloudy.sonata@gmail.com>
2015-05-20 15:13:12 -0400

    Comments update for WeatherContract.java

--- app/src/main/java/com/example/vinh/moonlight/data/WeatherContract.java
+++ app/src/main/java/com/example/vinh/moonlight/data/WeatherContract.java
@@ -25,10 +25,7 @@ import android.text.format.Time;
  * Defines table and column names for the weather database.
  */
 public class WeatherContract {
-    // The "Content authority" is a name for the entire content provider, similar to the
-    // relationship between a domain name and its website.  A convenient string to use for the
-    // content authority is the package name for the app, which is guaranteed to be unique on the
-    // device.
+    // Use the Content Authority as the name of the package of the App
     public static final String CONTENT_AUTHORITY = "com.example.vinh.moonlight";
 
     // Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
@@ -36,70 +33,99 @@ public class WeatherContract {
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
 
     // Possible paths (appended to base content URI for possible URI's)
-    // For instance, content://com.example.vinh.moonlight/weather/ is a valid path for
-    // looking at weather data. content://com.example.vinh.moonlight/givemeroot/ will fail,
-    // as the ContentProvider hasn't been given any information on what to do with "givemeroot".
-    // At least, let's hope not.  Don't be that dev, reader.  Don't be that dev.
+    // E.g. content://com.example.vinh.moonlight/weather/ for weather data
     public static final String PATH_WEATHER = "weather";
     public static final String PATH_LOCATION = "location";
 
-    // To make it easy to query for the exact date, we normalize all dates that go into
-    // the database to the start of the the Julian day at UTC.
+    /**
+     * Normalize dates for the database to the start of the the Julian day at UTC.
+     * @param startDate Regular date
+     * @return Normalized Julian day at UTC
+     */
     public static long normalizeDate(long startDate) {
-        // normalize the start date to the beginning of the (UTC) day
         Time time = new Time();
         time.set(startDate);
         int julianDay = Time.getJulianDay(startDate, time.gmtoff);
         return time.setJulianDay(julianDay);
     }
 
-    /*
-        Inner class that defines the table contents of the location table
+    /**
+     * Inner class that defines the table contents of the location table
+     * Table: ID | Location Settings | City Name | Longitude | Latitude
      */
-    //Table: ID | Location Settings | City Name | Longitude | Latitude
     public static final class LocationEntry implements BaseColumns {
+        /** ----------------  Uri definitions and functions ---------------- */
         public static final Uri CONTENT_URI =
                 BASE_CONTENT_URI.buildUpon().appendPath(PATH_LOCATION).build();
-
         public static final String CONTENT_TYPE =
                 ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION;
         public static final String CONTENT_ITEM_TYPE =
                 ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION;
 
+        /**
+         * Builds a Uri for the LocationEntry table with a specified ID
+         * @param id ID of Uri to build
+         * @return Resultant Uri
+         */
         public static Uri buildLocationUri(long id) {
             return ContentUris.withAppendedId(CONTENT_URI, id);
         }
 
+        /** ------------------------  Table Columns ------------------------ */
         public static final String TABLE_NAME = "location";
 
+        // Location setting; can be a ZIP code, postal code, address, city, etc.
         public static final String COLUMN_LOCATION_SETTING = "location_setting";
 
+        // The city of the location
         public static final String COLUMN_CITY_NAME = "city_name";
 
+        // Longitude coordinates of the location
         public static final String COLUMN_COORD_LONG = "longitude";
 
+        // Latitude coordinates of the location
         public static final String COLUMN_COORD_LAT = "latitude";
     }
 
-    /* Inner class that defines the table contents of the weather table */
+    /**
+     * Inner class that defines the table contents of the weather table
+     * Table: ID | Location Settings | City Name | Longitude | Latitude
+     */
     public static final class WeatherEntry implements BaseColumns {
+        /** ----------------  Uri definitions and functions ---------------- */
         public static final Uri CONTENT_URI =
                 BASE_CONTENT_URI.buildUpon().appendPath(PATH_WEATHER).build();
-
         public static final String CONTENT_TYPE =
                 ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_WEATHER;
         public static final String CONTENT_ITEM_TYPE =
                 ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_WEATHER;
 
-
+        /**
+         * Builds a Uri for the WeatherEntry table with a specified ID
+         * @param id ID of Uri to build
+         * @return Resultant Uri
+         */
         public static Uri buildWeatherUri(long id) {
             return ContentUris.withAppendedId(CONTENT_URI, id);
         }
 
+        /**
+         * Builds a Uri for the WeatherEntry table with a specified location setting
+         * E.g. locationSetting = "Toronto" builds CONTENT_URI/Toronto
+         * @param locationSetting Location for Uri to build
+         * @return Resultant Uri
+         */
         public static Uri buildWeatherLocation(String locationSetting) {
             return CONTENT_URI.buildUpon().appendPath(locationSetting).build();
         }
 
+        /**
+         * Builds a Uri for the WeatherEntry table with a specified location setting AND start date
+         * Uri returns weather for a certain number of upcoming days (up to ten)
+         * @param locationSetting Location for Uri to build
+         * @param startDate Starting date to get weather from
+         * @return Resultant Uri
+         */
         public static Uri buildWeatherLocationWithStartDate(
                 String locationSetting, long startDate) {
             long normalizedDate = normalizeDate(startDate);
@@ -107,19 +133,24 @@ public class WeatherContract {
                     .appendQueryParameter(COLUMN_DATE, Long.toString(normalizedDate)).build();
         }
 
+        /**
+         * Builds a Uri for the WeatherEntry table with a specified location setting AND exact date
+         * Uri returns weather for a single date
+         * @param locationSetting Location for Uri to build
+         * @param date Date to get weather from
+         * @return Resultant Uri
+         */
         public static Uri buildWeatherLocationWithDate(String locationSetting, long date) {
             return CONTENT_URI.buildUpon().appendPath(locationSetting)
                     .appendPath(Long.toString(normalizeDate(date))).build();
         }
 
-        public static String getLocationSettingFromUri(Uri uri) {
-            return uri.getPathSegments().get(1);
-        }
-
-        public static long getDateFromUri(Uri uri) {
-            return Long.parseLong(uri.getPathSegments().get(2));
-        }
-
+        /**
+         * Gets the starting date from a WeatherEntry Uri
+         * Related to function buildWeatherLocationWithStartDate()
+         * @param uri input Uri
+         * @return Starting date in long form
+         */
         public static long getStartDateFromUri(Uri uri) {
             String dateString = uri.getQueryParameter(COLUMN_DATE);
             if (null != dateString && dateString.length() > 0)
@@ -128,12 +159,34 @@ public class WeatherContract {
                 return 0;
         }
 
+        /**
+         * Gets the exact date from a WeatherEntry Uri
+         * Related to function buildWeatherLocationWithDate()
+         * @param uri input Uri
+         * @return Exact date in long form
+         */
+        public static long getDateFromUri(Uri uri) {
+            return Long.parseLong(uri.getPathSegments().get(2));
+        }
+
+        /**
+         * Gets the location setting from a WeatherEntry Uri
+         * @param uri input Uri
+         * @return Location setting in String form
+         */
+        public static String getLocationSettingFromUri(Uri uri) {
+            return uri.getPathSegments().get(1);
+        }
+
+        /** ------------------------  Table Columns ------------------------ */
         public static final String TABLE_NAME = "weather";
 
         // Column with the foreign key into the location table.
         public static final String COLUMN_LOC_KEY = "location_id";
+
         // Date, stored as long in milliseconds since the epoch
         public static final String COLUMN_DATE = "date";
+
         // Weather id as returned by API, to identify the icon to be used
         public static final String COLUMN_WEATHER_ID = "weather_id";
 
